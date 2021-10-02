b451394ab95cb017ae69a769a6d6362028313b0e
nogamishinpei <nogamichiru@gmail.com>
2021-10-02 01:57:54 +0000

    [Add]インデント調整

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,6 @@
 class Relationship < ApplicationRecord
-  #アソシエーション
+  # アソシエーション
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-  #class_name: "User"の定義でUserテーブルのレコードを参照する。
-
-
+  # class_name: "User"の定義でUserテーブルのレコードを参照する。
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,27 +6,25 @@ Rails.application.routes.draw do
   get 'homes/about'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
 
     resources :post_comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:index, :show, :edit, :update] do
-
-  #フォローフォロワー機能のルーティング
-  resource :relationships, only: [:create, :destroy]
-  get 'followings' => 'relationships#followings', as: 'followings'
-  get 'followers' => 'relationships#followers', as: 'followers'
-  get "relationships/followings"
+    # フォローフォロワー機能のルーティング
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+    get "relationships/followings"
   end
 
-  #お問い合わせフォームのルーティング
+  # お問い合わせフォームのルーティング
   resources :contacts, only: [:new, :create]
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
 
-  #検索機能のルーティング
+  # 検索機能のルーティング
   get 'search' => 'searchs#search'
-
 end
